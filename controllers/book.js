e71b4d5ddefbbe7df28338b5900b2fe787f1aa90
Nandini Hariprasad <nharipr2@my.centennialcollege.ca>
2021-10-29 01:08:41 -0400

    Updated Book Controller

--- controllers/book.js
+++ controllers/book.js
@@ -41,32 +41,130 @@ module.exports.details = (req, res, next) => {
     });
 }
 
+// Gets a book by id and renders the details page.
+module.exports.details = (req, res, next) => {
+    
+    let id = req.params.id;
+
+    Book.findById(id, (err, bookToShow) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('book/details', {
+                title: 'Book Details', 
+                book: bookToShow
+            })
+        }
+    });
+}
+
 // Renders the Add form using the add_edit.ejs template
 module.exports.displayAddPage = (req, res, next) => {
-
+    
+    res.render('book/add', {title: 'Add a new Book'});     
 
 }
 
 // Processes the data submitted from the Add form to create a new book
 module.exports.processAddPage = (req, res, next) => {
 
+    let newBook = Book({
+        "Title": req.body.Title,
+        "Author":req.body.Author,
+        "Genre":req.body.Genre,
+        "Description":req.body.Description,
+        "Price":req.body.Price
+    });
+
+    Book.create(newBook, (err, Book) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the book list
+            res.redirect('/book/list');
+        }
+
+    });
 
 }
 
 // Gets a book by id and renders the Edit form using the add_edit.ejs template
 module.exports.displayEditPage = (req, res, next) => {
     
+    let id = req.params.id;
+
+    Book.findById(id, (err, bookToEdit) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('book/edit', {title: 'Edit Book', book: bookToEdit})
+        }
+
+    });
 
 }
 
 // Processes the data submitted from the Edit form to update a book
 module.exports.processEditPage = (req, res, next) => {
     
+    let id = req.params.id;
+
+    let updateBook = Book({
+        "_id": id,
+        "Title": req.body.Title,
+        "Author":req.body.Author,
+        "Genre":req.body.Genre,
+        "Description":req.body.Description,
+        "Price":req.body.Price
+
+    });
+    Book.updateOne({_id: id}, updateBook, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the book list
+            res.redirect('/book/list');
+        }
+    
+    });
     
 }
 
 // Deletes a book based on its id.
 module.exports.performDelete = (req, res, next) => {
     
+    let id = req.params.id;
+
+    Book.remove({_id: id}, (err) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the book list
+            res.redirect('/book/list');
+        }
+
+    });
 
 }
